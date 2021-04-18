d05a4f3d75d7b39ff3a2ba217971807a0522477f
TomasSalas <tsalasarancibia@gmail.com>
2021-04-17 21:06:29 -0400

    Primera Parte

--- festival/src/App.js
+++ festival/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./IMG/logo.png";
+import "./header.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Header() {
+    return (
+        <div className="Header">
+            <div className="header_menu">
+                <nav className="navbar navbar-expand-lg">
+                    <div className="container">
+                        <a className="navbar-brand" href="/"><img src={logo}  className="logo-brand" alt="logo"/></a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <ion-icon name="menu-outline"></ion-icon>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Bienvenido</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Conocenos</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Galeria</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Compañia De Teatro</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/">Contacto</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>  
+            </div>
+    
     </div>
-  );
+    )
 }
 
-export default App;
+export default Header
+
