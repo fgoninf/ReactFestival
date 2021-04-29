090a9afe35429330d44a08dcd698f36b4ec5e46c
TomasSalas <tsalasarancibia@gmail.com>
2021-04-29 15:14:12 -0400

    Contact Success

--- festival/src/Contacto.js
+++ festival/src/Contacto.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import emailjs from "emailjs-com";
+import Swal from 'sweetalert2';
 function Contacto() {
 
     function sendEmail(e){
-                    
+        emailjs.sendForm('gmail', 'template_ukh2r6n', e.target, 'user_AehHLruJFurZE84Qk85Jh')
+        .then(function(response) {
+           
+            Swal.fire(
+                'Good job!',
+                'You clicked the button!',
+                'success'
+            );
+        }, function(error) {
+           console.log('FAILED...', error);
+        });
+        e.target.reset();
     }
     return (
         <div >
@@ -17,28 +29,28 @@ function Contacto() {
                                 <br></br>
                                 <div className="control-group">
                                     <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                                        <input className="form-control" id="name" type="text" placeholder="Nombre Y Apellido" required="required" data-validation-required-message="Por favor Ingrese Datos Solicitados." />
+                                        <input className="form-control" name="name" type="text" placeholder="Nombre Y Apellido" required="required" data-validation-required-message="Por favor Ingrese Datos Solicitados." />
                                         <p className="help-block text-danger"></p>
                                     </div>
                                 </div>
                                 <br></br>
                                 <div className="control-group">
                                     <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                                        <input className="form-control" id="email" type="email" placeholder="Correo Electronico" required="required" data-validation-required-message="Por favor Ingrese Datos Solicitados." />
+                                        <input className="form-control" name="email" type="email" placeholder="Correo Electronico" required="required" data-validation-required-message="Por favor Ingrese Datos Solicitados." />
                                         <p className="help-block text-danger"></p>
                                      </div>
                                 </div>
                                 <br></br>
                                 <div className="control-group">
                                     <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                                        <input className="form-control" id="phone" type="tel" placeholder="Numero De Contacto" required="required" data-validation-required-message="Por favor Ingrese Datos Solicitados." />
+                                        <input className="form-control" name="phone" type="tel" placeholder="Numero De Contacto" required="required" data-validation-required-message="Por favor Ingrese Datos Solicitados." />
                                         <p className="help-block text-danger"></p>
                                     </div>
                                 </div>
                                 <br></br>
                                 <div className="control-group">
                                     <div className="form-group floating-label-form-group controls mb-0 pb-2">
-                                        <textarea className="form-control" id="message" rows="5" placeholder="Mensaje" required="required" data-validation-required-message="Por favor Ingrese Datos Solicitados."></textarea>
+                                        <textarea className="form-control" name="message" rows="5" placeholder="Mensaje" required="required" data-validation-required-message="Por favor Ingrese Datos Solicitados."></textarea>
                                         <p className="help-block text-danger"></p>
                                     </div>
                                 </div>
