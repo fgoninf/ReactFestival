59edc4098c8d018338ab2f2db49d1f89eeac3e70
TomasSalas <57121296+TomasSalas@users.noreply.github.com>
2021-05-10 00:34:16 -0400

    update correo

--- festival/src/api/contact.js
+++ festival/src/api/contact.js
@@ -15,11 +15,12 @@ export default async (req, res) => {
   try {
     const emailRes = await transporter.sendMail({
     from: email,
-    to: 'festivalteatrobuin@gmail.com',
-    subject: `Envío de formulario de contacto desde ${name}`,
+    to: 'tsalasarancibia@gmail.com',
+    subject: `Envío de formulario de contacto desde`,
     html: `<p>Tiene un nuevo envío de formulario de contacto</p><br>
     <p><strong>Name: </strong> ${name} </p><br>
     <p><strong>Phone: </strong> ${phone} </p><br>
+    <p><strong>Email: </strong> ${email} </p><br>
     <p><strong>Message: </strong> ${message} </p><br>
 
     `,
